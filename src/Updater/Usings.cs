9bdf90a951bc338ba4fff1ab1ed3c576dc4920b9
ziyuhaokun <ziyuhaokun@qq.com>
2023-09-15 14:45:32 +0800

    添加项目文件。

---
+++ src/Updater/Usings.cs
@@ -0,0 +1,8 @@
+﻿global using System;
+global using System.Windows;
+
+global using CommunityToolkit.Mvvm.ComponentModel;
+global using CommunityToolkit.Mvvm.Input;
+
+global using Wpf.Ui.Contracts;
+global using Wpf.Ui.Services;
---
+++ src/Updater/ViewModels/Pages/DataViewModel.cs
@@ -0,0 +1,24 @@
+﻿
+using Wpf.Ui.Controls;
+
+namespace Updater.ViewModels.Pages
+{
+    public partial class DataViewModel : ObservableObject, INavigationAware
+    {
+        private bool _isInitialized = false;
+
+        public void OnNavigatedTo()
+        {
+            if (!_isInitialized)
+                InitializeViewModel();
+        }
+
+        public void OnNavigatedFrom() { }
+
+        private void InitializeViewModel()
+        {
+
+            _isInitialized = true;
+        }
+    }
+}
